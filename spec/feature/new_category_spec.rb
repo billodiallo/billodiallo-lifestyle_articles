6f68a23ac0502e662da4e25540d1482b2837ffc3
Billo Diallo <billodiallonet@gmail.com>
2021-04-14 10:50:00 +0300

    Test second  new category creation feature

--- spec/feature/new_category_spec.rb
+++ spec/feature/new_category_spec.rb
@@ -16,5 +16,18 @@ RSpec.describe 'the signin process', type: :system do
     click_button 'create'
     expect(page).to have_text('category created')
   end
+
+  it 'Create category' do
+    user2 = User.create(name: 'mabillo', email: 'mbillo@email.com', password: '123456')
+    visit root_path
+    click_link_or_button 'Sign in'
+    fill_in 'user[email]', with: user2.email
+    fill_in 'user[password]', with: '123456'
+    click_button 'Log in'
+    click_link_or_button 'New Category'
+    fill_in 'name', with: ''
+    click_button 'create'
+    expect(page).not_to have_text('category created')
+  end
 end
 
