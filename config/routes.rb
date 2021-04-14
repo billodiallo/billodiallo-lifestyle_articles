779bb42f431ca5fab88925601c1d1ec5e7916e15
Billo Diallo <billodiallonet@gmail.com>
2021-04-14 07:49:29 +0300

    Update route fo sign out

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :articles, only: [:new, :create, :show] do
     resources :votes, only: %i[create destroy]
   end
-  devise_for :users
+  devise_for :users do
+  end
   resources :categories, only: %i[new create show index]
   root 'categories#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
