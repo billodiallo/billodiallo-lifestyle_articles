758867fff83d2426a9c80819ab23b2e1da7652ac
Billo Diallo <billodiallonet@gmail.com>
2021-04-14 09:19:45 +0300

    testing feature article creation with image uploading

--- spec/feature/new_article_spec.rb
+++ spec/feature/new_article_spec.rb
@@ -17,4 +17,23 @@ RSpec.describe 'Create article proccess', type: :system do
       expect(page).to have_text('category created')
     end
 
+    it 'Create article' do
+      user1 = User.create(name: 'billo', email: 'billo@email.com', password: '123456')
+      visit root_path
+      click_link_or_button 'Sign in'
+      fill_in 'user[email]', with: user1.email
+      fill_in 'user[password]', with: '123456'
+      click_button 'Log in'
+      click_link_or_button 'New Category'
+      fill_in 'name', with: 'liverpool'
+      click_button 'create'
+      expect(page).to have_text('category created')
+      click_link_or_button 'New Game'
+      fill_in 'Title', with: 'champions league'
+      fill_in 'Text', with: ' will win again Real madrid'
+      attach_file 'Image', "#{Rails.root}/app/assets/images/seriea.jpg"
+      click_button 'create'
+      expect(page).to have_text('Your Game article created!')
+    end
+
 end
