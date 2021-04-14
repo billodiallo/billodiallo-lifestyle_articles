53a83f0537b7341f6355c08b90188eca80daa9ed
Billo Diallo <billodiallonet@gmail.com>
2021-04-14 08:49:29 +0300

    Test category

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  let(:category) { Category.create(name: 'Man city',priority:'2')}
+  context 'should create new categorie successfully' do
+    it 'should return categorie name' do
+      expect(category.name).to eq('Man city')
+    end
+    it 'should return priority' do
+        expect(category.priority).to eq(2)
+      end
+end
+
+describe 'ActiveRecord associations' do
+    it 'has many votes' do
+      expect { should has_many(votes) }
+    end
+    it 'has many articles' do
+      expect { should has_many(articles).with_foreign_key }
+    end
+end
+end
+
+
+
